a70fab92b19ecb5191080a215e99c6d0cf1fb2d7
Sol Battaglia <solbattaglia@gmail.com>
2022-06-03 10:58:38 -0300

    Added connection to database

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,11 +4,17 @@ import (
 	"database/sql"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/SolBaa/authservice/data"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 const webPORT = "8080"
+
+var count int
 
 type Config struct {
 	DB     *sql.DB
@@ -17,9 +23,17 @@ type Config struct {
 
 func main() {
 	log.Println("Starting auth server on port", webPORT)
-	//TODO congif DB
+	// congif DB
+	conn := connectToDB()
+	if conn == nil {
+		log.Println("Failed to connect to database. Exiting...")
+		os.Exit(1)
+	}
 
-	app := Config{}
+	app := Config{
+		DB:     conn,
+		Models: data.New(conn),
+	}
 
 	srv := &http.Server{
 		Addr:    ":" + webPORT,
@@ -29,5 +43,40 @@ func main() {
 	err := srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func connectToDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Failed to connect to database. Retrying...")
+			count++
+
+		} else {
+			log.Println("Connected to database")
+			return connection
+		}
+		if count > 10 {
+			log.Println("Failed to connect to database. Exiting...")
+			os.Exit(1)
+		}
+		log.Println("Failed to connect to database. Retrying...")
+		time.Sleep(time.Second * 2)
+		continue
 	}
 }
--- go.mod
+++ go.mod
@@ -6,5 +6,8 @@ require (
 	github.com/go-chi/chi v4.1.2+incompatible
 	github.com/go-chi/chi/v5 v5.0.7 // indirect
 	github.com/go-chi/cors v1.2.1
+	github.com/jackc/pgx v3.6.2+incompatible // indirect
+	github.com/jackc/pgx/v4 v4.16.1
+	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e
 )
